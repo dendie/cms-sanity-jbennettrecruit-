6798f300a4e841ffa80682154a27791823b0d16b
Dendie <lokassd@gmail.com>
2024-07-08 12:07:50 +0700

    sanity first setup

---
+++ schemaTypes/aboutUs.ts
@@ -0,0 +1,58 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'aboutUs',
+  title: 'About Us',
+  type: 'document',
+  fields: [
+        defineField({
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        }),
+        defineField({
+            name: 'subtitle',
+            title: 'Subtitle',
+            type: 'string',
+        }),
+        defineField({
+            name: 'button',
+            title: 'Button',
+            type: 'object',
+            fields: [
+                {
+                    name: 'href',
+                    title: 'Link',
+                    type: 'url',
+                },
+                {
+                    name: 'name',
+                    type: 'string',
+                    title: 'Name'
+                },
+            ]
+        }),
+        defineField({
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 96,
+            },
+        }),
+        defineField({
+            name: 'body',
+            title: 'Body',
+            type: 'blockContent',
+        }),
+        defineField({
+            name: 'mainImage',
+            title: 'Main image',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        }),
+    ]
+})
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,10 @@
-export const schemaTypes = []
+import aboutUs from './aboutUs'
+import blackBox from './blackBox'
+import blockContent from './blockContent'
+import clientLogin from './clientLogin'
+import contactUs from './contactUs'
+import home from './home'
+import ourPolicies from './ourPolicies'
+import specializations from './specializations'
+
+export const schemaTypes = [aboutUs, blackBox, blockContent, clientLogin, contactUs, home, ourPolicies, specializations]
